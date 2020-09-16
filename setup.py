aa83865c2435942e38e90e441018014db2c474af
Tiago Ribeiro <tiago.astro@gmail.com>
2020-09-15 22:22:51 -0700

    Update setup.py and conda/meta.yaml to add rotator schema. Update Jenkinsfile.conda to configure ts_config_mttcs package.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup, find_namespace_packages
+import os
+import sys
+import pathlib
+import setuptools
 
 install_requires = []
 tests_require = ["pytest", "pytest-cov", "pytest-flake8", "asynctest"]
@@ -8,8 +11,11 @@ __all__ = ["__version__"]
 
 __version__ = "{version}"
 """
+tools_path = pathlib.PurePosixPath(setuptools.__path__[0])
+base_prefix = pathlib.PurePosixPath(sys.base_prefix)
+data_files_path = tools_path.relative_to(base_prefix).parents[1]
 
-setup(
+setuptools.setup(
     name="ts_hexrotcomm",
     description="Python code for communicating with and emulating the low-level main telescope "
     "camera and hexapod controllers written by Moog",
@@ -20,8 +26,9 @@ setup(
     setup_requires=["setuptools_scm", "pytest-runner"],
     install_requires=install_requires,
     package_dir={"": "python"},
-    packages=find_namespace_packages(where="python"),
+    packages=setuptools.find_namespace_packages(where="python"),
     package_data={"": ["*.rst", "*.yaml"]},
+    data_files=[(os.path.join(data_files_path, "schema"), ["schema/Rotator.yaml"])],
     tests_require=tests_require,
     extras_require={"dev": dev_requires},
     license="GPL",
