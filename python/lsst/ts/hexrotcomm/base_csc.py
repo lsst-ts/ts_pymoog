3a1010260f1f73a2b2adb2624af648353be0b4ac
Russell Owen <rowen@uw.edu>
2021-11-18 17:31:47 -0800

    BaseCsc: remove schema_path constructor argument
    it is deprecated was not being used.

--- python/lsst/ts/hexrotcomm/base_csc.py
+++ python/lsst/ts/hexrotcomm/base_csc.py
@@ -132,17 +132,8 @@ class BaseCsc(salobj.ConfigurableCsc):
         Configuration structure.
     TelemetryClass : `ctypes.Structure`
         Telemetry structure.
-    schema_path : `str`, `pathlib.Path` or `None`, optional
-        Path to a schema file used to validate configuration files.
-        This is deprecated; new code should specify ``config_schema`` instead.
-        The recommended path is ``<package_root>/"schema"/f"{name}.yaml"``
-        for example:
-
-            schema_path = pathlib.Path(__file__).resolve().parents[4] \
-                / "schema" / f"{name}.yaml"
     config_schema : `dict` or None, optional
         Configuration schema, as a dict in jsonschema format.
-        Exactly one of ``schema_path`` or ``config_schema`` must not be None.
     config_dir : `str`, optional
         Directory of configuration files, or None for the standard
         configuration directory (obtained from `_get_default_config_dir`).
@@ -196,7 +187,6 @@ class BaseCsc(salobj.ConfigurableCsc):
         CommandCode,
         ConfigClass,
         TelemetryClass,
-        schema_path=None,
         config_schema=None,
         config_dir=None,
         initial_state=salobj.State.STANDBY,
@@ -237,7 +227,6 @@ class BaseCsc(salobj.ConfigurableCsc):
         super().__init__(
             name=name,
             index=index,
-            schema_path=schema_path,
             config_dir=config_dir,
             config_schema=config_schema,
             initial_state=initial_state,
